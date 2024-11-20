a160ed8eb71c3511bcdce04ef82ca1a1b97e32d6
Saivathsal <vathsalsai133@gmail.com>
2024-11-20 11:05:49 +0530

    Completed

---
+++ src/main/java/Sai/Maven_java_project/App.java
@@ -0,0 +1,10 @@
+package Sai.Maven_java_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
