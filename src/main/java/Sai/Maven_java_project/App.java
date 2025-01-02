93d31617d051039a9ca32adc97cdef89f3b8e6ff
saivathsal <vathsalsai133@gmail.com>
2025-01-02 07:39:34 +0530

    Update App.java

--- src/main/java/Sai/Maven_java_project/App.java
+++ src/main/java/Sai/Maven_java_project/App.java
@@ -5,6 +5,6 @@ package Sai.Maven_java_project;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Bye");
     }
 }
